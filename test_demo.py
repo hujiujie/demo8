a80daf9fa74ad811deea6af9311ae31cfb4f1e15
hujiujie <626009680@qq.com>
2019-05-24 14:41:31 +0800

    update

---
+++ test_demo.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/24 12:15
+# @Author  : hjj
+# @Site    : 
+# @File    : test_demo.py
+
+import pytest
+
+def test_demo1():
+    assert 1==1
+
+
+def test_demo2():
+    assert 1==4
+
+
