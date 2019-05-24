913682dc5aadf04404e66b8f1aad8ca2d6194847
hujiujie <626009680@qq.com>
2019-05-24 15:09:58 +0800

    新增接口

--- test_demo.py
+++ test_demo.py
@@ -9,8 +9,13 @@ import pytest
 def test_demo1():
     assert 1==1
 
-
 def test_demo2():
     assert 1==4
+
+def test_demo3():
+    assert 2 == 2
+
+def test_demo4():
+    assert "test" == "test"
 
 
